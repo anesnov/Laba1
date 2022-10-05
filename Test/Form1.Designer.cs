97bfb41b1ca57bd8c01b735af55aa6f2df23efb0
anesnov <61014956+aleteklas@users.noreply.github.com>
2022-10-05 18:02:56 +0300

    Добавьте файлы проекта.

---
+++ Test/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿using System.Windows.Forms;
+
+namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Вход = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Выход1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Выход2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Вход,
+            this.Выход1,
+            this.Выход2});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(343, 447);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Вход
+            // 
+            this.Вход.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Вход.HeaderText = "Вход";
+            this.Вход.Name = "Вход";
+            this.Вход.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Выход1
+            // 
+            this.Выход1.HeaderText = "Выход 1";
+            this.Выход1.Name = "Выход1";
+            this.Выход1.ReadOnly = true;
+            this.Выход1.ToolTipText = "Метод “пузырька” ";
+            // 
+            // Выход2
+            // 
+            this.Выход2.HeaderText = "Выход 2";
+            this.Выход2.Name = "Выход2";
+            this.Выход2.ReadOnly = true;
+            this.Выход2.ToolTipText = "Метод вставки с бинарным включением";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(358, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(280, 39);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Метод “пузырька” ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(358, 133);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(280, 58);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Метод вставки с бинарным включением";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(354, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Количество шагов:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(354, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Количество шагов:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(354, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Время:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(354, 248);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Время:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(358, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(434, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Сортировки";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Вход;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Выход1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Выход2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Label label3;
+        private Label label4;
+        private Button button1;
+        private Button button2;
+    }
+}
+
---
+++ Test/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.dataGridView1.DataError += new DataGridViewDataErrorEventHandler(dataGridView1_DataError);
+            this.Вход.ValueType = typeof(int);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+                
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.OpenFile();
+        }
+
+
+        private void Clear(int col)                 //Очистка столбца
+        {
+            for(int i=0; i<dataGridView1.RowCount-1; i++)
+            {
+                dataGridView1.Rows[i].Cells[col].Value = null;
+            }
+        }
+
+        private void dataGridView1_DataError(object sender, DataGridViewDataErrorEventArgs anError)
+        {
+            dataGridView1.RefreshEdit();
+            anError.ThrowException = false;            
+        }         
+
+        private void button3_Click(object sender, EventArgs e)        //Пузырёк
+        {
+
+            void Swap(ref int e1, ref int e2)
+            {
+                var temp = e1;
+                e1 = e2;
+                e2 = temp;
+            }
+
+            Clear(1);
+
+            int[] array = new int[dataGridView1.RowCount - 1];
+            int nulls = 0;
+            int len = dataGridView1.RowCount - 1;
+
+            for (int i = 0; i<len; i++)             //Копирование значений первой колонки в массив
+            {
+                if (dataGridView1.Rows[i].Cells[0].Value != null && dataGridView1.Rows[i].Cells[0].Value.ToString() != string.Format(""))
+                {
+                    array[i-nulls] = int.Parse(dataGridView1.Rows[i].Cells[0].Value.ToString());
+                }
+                else nulls++;
+
+            }
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            len = len - nulls;
+            int count = 0;
+            for (var i = 1; i < len; i++)           // Сортировка
+            {
+                for (var j = 0; j < len - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(ref array[j], ref array[j + 1]);
+                        count++;
+                    }
+                    
+                }
+            }
+            stopwatch.Stop();
+
+            for (int i = 0; i < len; i++)           // Вывод во второй столбец
+            {
+                dataGridView1.Rows[i].Cells[1].Value = array[i];
+            }
+
+            label3.Text = "Время: " + stopwatch.Elapsed;
+            label1.Text = "Количество шагов: " + count;            
+        }
+
+        private void button4_Click(object sender, EventArgs e)        //Метод вставки с бинарным включением
+        {            
+            Clear(2);
+
+            int[] array = new int[dataGridView1.RowCount - 1];
+            int nulls = 0;
+            int len = dataGridView1.RowCount - 1;
+
+            for (int i = 0; i < len; i++)             //Копирование значений первой колонки в массив
+            {
+                if (dataGridView1.Rows[i].Cells[0].Value != null && dataGridView1.Rows[i].Cells[0].Value.ToString() != string.Format(""))
+                {
+                    array[i - nulls] = int.Parse(dataGridView1.Rows[i].Cells[0].Value.ToString());
+                }
+                else nulls++;
+
+            }
+
+            len = len - nulls;
+            int count = 0;
+            //int key, lo, hi, mid = 0;
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                int x = array[i];
+
+                int j = Math.Abs(
+                    Array.BinarySearch(array,
+                                       0, i, x) + 1);
+
+                System.Array.Copy(array, j,
+                                  array, j + 1,
+                                  i - j);
+
+                array[j] = x;
+                count++;
+            }
+            stopwatch.Stop();
+
+            for (int i = 0; i < len; i++)           // Вывод во второй столбец
+            {
+                dataGridView1.Rows[i].Cells[2].Value = array[i];
+            }
+
+            label2.Text = "Количество шагов: " + count;
+            label4.Text = "Время: " + stopwatch.Elapsed;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            foreach (DataGridViewColumn col in dataGridView1.Columns)
+            {
+                dt.Columns.Add(col.Name);
+            }
+
+            //foreach (DataGridViewRow row in dataGridView1.Rows)
+            //{
+            //    DataRow dRow = dt.NewRow();
+            //    foreach (DataGridViewCell cell in row.Cells)
+            //    {
+            //        dRow[cell.ColumnIndex] = cell.Value;
+            //    }
+            //    dt.Rows.Add(dRow);
+            //};
+
+            object[] cellValues = new object[dataGridView1.Columns.Count];
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                for (int i = 0; i < row.Cells.Count; i++)
+                {
+                    cellValues[i] = row.Cells[i].Value;
+                }
+                dt.Rows.Add(cellValues);
+            }
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "XML|*.xml";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    DataSet ds = new DataSet();
+                    ds.Tables.Add(dt);
+                    XmlTextWriter xmlSave = new XmlTextWriter(sfd.FileName, Encoding.UTF8);
+                    xmlSave.Formatting = Formatting.Indented;
+                    ds.DataSetName = "Data";
+                    ds.Tables[0].WriteXml(xmlSave);
+                    //dt.WriteXml(sfd.FileName);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "XML|*.xml";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    XmlReader xmlFile = XmlReader.Create(ofd.FileName, new XmlReaderSettings());
+                    ds.ReadXml(xmlFile);
+                    dataGridView1.DataSource = ds.Tables[0].DefaultView; //
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+    }
+}
